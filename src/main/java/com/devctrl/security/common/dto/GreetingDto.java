38a8b328d1b79e1c0955fae0d6334758850b6b2f
Rob Kovtiuk <robsonheater@gmail.com>
2018-03-03 22:27:04 +0200

    Common -> add DTOs

---
+++ src/main/java/com/devctrl/security/common/dto/GreetingDto.java
@@ -0,0 +1,9 @@
+package com.devctrl.security.common.dto;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class GreetingDto {
+    private long id;
+    private String content;
+}
